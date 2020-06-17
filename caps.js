358ba9b4a0d5d4a2f88a27b4fba907c3e42d2deb
israaothman <israaayoub@hotmail.com>
2020-06-17 14:32:17 +0300

    edite object

--- caps.js
+++ caps.js
@@ -14,7 +14,7 @@ events.on('delivered',paylaod => globalLog('delivered',paylaod));
 function globalLog(event,paylaod){
   let time = new Date();
   let output = {event: event ,time: time, payload:paylaod};
-  console.log(`EVENT ${output}`);
+  console.log('EVENT' , output);
 }
 
 require('./vendor'); //emit pickup
